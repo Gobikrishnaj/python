7935db614797764c7381f4c5e88894202caa5b09
Gobikrishnaj <231001045@rajalakshmi.edu.in>
2024-06-15 15:09:37 +0530

    Week-10.03.py

--- Week-10/Week-10.03.py
+++ Week-10/Week-10.03.py
@@ -17,7 +17,7 @@ The first line contains a single integer n , the length of A .
 The second line contains n space-separated integers,A[i].
 
 
-
+ 
 Output Format
 
 Print peak numbers separated by space.
